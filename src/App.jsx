830a72ee3803306fd6363fb0ceb2afb53b7caad6
ejaymedia <eliafoeri@gmail.com>
2025-12-31 13:06:26 +0100

    registeration page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Login from "./pages/Login";
+// import Register from "./pages/Register";
 import HostelDetails from "./pages/HostelDetails";
 import StudentDashboard from "./pages/StudentDashboard";
 import LandlordDashboard from "./pages/LandlordDashboard";
@@ -10,6 +11,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      {/* <Route path="/register" element={<Register />} /> */}
       <Route path="/hostel-details" element={<HostelDetails />} />
       <Route path="/dashboard" element={<StudentDashboard />}/>
       <Route path="/landlord-dashboard" element={<LandlordDashboard />}/>
